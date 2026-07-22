8c82f1ae1067db03ec5316a4aab2b68d82cae49f
Tsion <tsionshimelis3@gmail.com>
2026-07-22 16:13:41 -0700

    update account.py

--- day04/account.py
+++ day04/account.py
@@ -1,6 +1,5 @@
 class Account:
-
-    def __init__(self, owner, account_number, balance=0):
+    def __init__(self, owner, account_number, balance):
         self.owner = owner
         self.account_number = account_number
         self.__balance = balance
@@ -13,17 +12,25 @@ class Account:
         if amount > 0:
             self.__balance += amount
         else:
-            print("Deposit amount must be positive")
+            print("Invalid deposit amount.")
 
     def withdraw(self, amount):
-        if amount > self.__balance:
-            print("Insufficient funds")
-        elif amount < 0:
-            print("Withdrawal amount must be positive")
+        if amount <= 0:
+            print("Invalid withdrawal amount.")
+        elif amount > self.__balance:
+            print("Insufficient funds.")
         else:
             self.__balance -= amount
 
-    def statement(self):
-        print("Owner:", self.owner)
-        print("Account Number:", self.account_number)
-        print("Balance:", self.balance)
+
+account1 = Account("Almaz", "1001", 1000)
+account2 = Account("Dawit", "1002", 500)
+
+account1.deposit(500)
+account1.withdraw(200)
+
+account2.deposit(300)
+account2.withdraw(1000)
+
+print(account1.owner, account1.balance)
+print(account2.owner, account2.balance)
